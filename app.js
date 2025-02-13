cdccd26c98d5581fb842936cc3feca78eb86c125
nodmalikov <abcmalikov@gmail.com>
2025-02-13 18:41:50 +0500

    Customize desktop and mobile menu

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const accordionHeads = document.querySelectorAll('.accordion__head');
     const accordionBodies = document.querySelectorAll('.accordion__body');
     const accordionBadges = document.querySelectorAll('.accordion__badge');
+    const sitenavList = document.querySelector('.sitenav__list');
+    const siteHeader = document.querySelector('.site-header');
+    const mainContent = document.querySelector('.main-content');
     
     menuButton.addEventListener('click', (event) => {
         event.stopPropagation();
@@ -17,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     
+    // ACCORDION
     accordionHeads.forEach((item, index) => {
         item.addEventListener('click', () => {
             const isActive = accordionBodies[index].classList.contains('accordion-active');
@@ -36,4 +40,27 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+    
+    // SCROLL
+    window.onscroll = function() {
+        if (window.innerWidth > 1001) {
+            const offset = sitenavList.offsetHeight;
+            if (window.scrollY > offset - 20) {
+                sitenavList.classList.add('sitenav-bg');
+            } else if (window.scrollY < offset - 20) {
+                sitenavList.classList.remove('sitenav-bg');
+            }
+        }
+    }
+    
+    //MAIN CONTENT'S MARGIN-TOP
+    if ( window.innerWidth <= 1050) {
+        function updateMainContentMargin() {
+            const headerHeight = siteHeader.offsetHeight;
+            mainContent.style.marginTop = `${headerHeight}px`;
+        }
+    }
+    
+    updateMainContentMargin();
+    new ResizeObserver(updateMainContentMargin).observe(elHeader);
 });
